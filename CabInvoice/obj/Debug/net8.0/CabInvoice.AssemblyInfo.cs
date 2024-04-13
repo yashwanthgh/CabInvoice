82042bf136954e279ccf4a541949d46f5c461aa9
saikatCV <saikat030703@gmail.com>
2024-04-13 09:37:17 +0530

    pulling the data form uc4

--- CabInvoice/obj/Debug/net8.0/CabInvoice.AssemblyInfo.cs
+++ CabInvoice/obj/Debug/net8.0/CabInvoice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CabInvoice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd107e9eb57dad70b04a5449dd24043fef2c3e20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b35ab0e1cfd56280c5746283cdc1efacfac2524")]
 [assembly: System.Reflection.AssemblyProductAttribute("CabInvoice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CabInvoice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
